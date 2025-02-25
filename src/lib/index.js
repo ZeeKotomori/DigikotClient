b47e88d03382601f830948e515ea5844f7401f80
ZeeKotomori <zikriaf23@gmail.com>
2025-02-25 20:09:50 +0700

    initial commit

---
+++ src/lib/index.js
@@ -0,0 +1,3 @@
+// place files you want to import through the `$lib` alias in this folder.
+import Login from '$lib/components/login.svelte'
+import "../app.css";
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import Login from '$lib/components/login.svelte';
+    import "../../app.css";
+</script>
+
+<Login></Login>
