5a2ce189a9d9c4d6180669ce497f94e64a247b87
ZeeKotomori <zikriaf23@gmail.com>
2025-02-27 00:44:02 +0700

    add startup page component with navigation to registration

---
+++ src/lib/components/startUpPage.svelte
@@ -0,0 +1,18 @@
+<script>
+    import BtnMove from "./button/BtnMove.svelte";
+    import { goto } from "$app/navigation";
+</script>
+
+<style>
+    .image {
+        width: 200px;
+        height: auto;
+        margin-bottom: 40px;
+    }
+</style>
+
+<div class="container flex flex-col items-center justify-between bg-white text-center h-screen py-6 px-10">
+    <img src="/Digikot_Horizontal_Logo.svg" alt="Digikot Logo" class="w-20 h-auto mb-5" />
+    <img src="/Illustration_Angkot.png" alt="Angkot Illustration" class="image" />
+    <BtnMove to="/register" label="Continue With Phone" className="bg-green-600 text-white px-4 py-2 rounded-xl text-lg w-full max-w-xs"/>
+</div>
