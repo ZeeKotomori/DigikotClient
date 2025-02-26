aca19174fa8872de1451c5578102d1142ae498b3
ZeeKotomori <zikriaf23@gmail.com>
2025-02-27 00:44:31 +0700

    add BtnMove component for navigation with customizable label and class

---
+++ src/lib/components/button/BtnMove.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { goto } from "$app/navigation";
+    
+    export let to = "/";
+    export let label = "";
+    export let className = "";
+
+    const navigate = () => {
+        console.log("Navigating to", to);
+        goto(to);
+    }
+</script>
+
+<button class={className} on:click={navigate}>
+    {label}
+</button>
